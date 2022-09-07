9de07999a73895bd6a129d001ef3570457b81abc
Alejandro <alejandrom.montoya@udea.edu.co>
2022-09-07 11:15:05 -0500

    fp reader

---
+++ things/fingerprint_mqtt/config.h
@@ -0,0 +1,7 @@
+const char* ssid     = "WIFI-LIS";
+const char* password = "";
+const char* hostname = "ESP32_1";
+
+IPAddress ip(192, 168, 0, 112);
+IPAddress gateway(192, 168, 0, 1);
+IPAddress subnet(255, 255, 255, 0);
---
+++ things/fingerprint_mqtt/mqtt.hpp
@@ -0,0 +1,57 @@
+const char* MQTT_BROKER_ADRESS = "192.168.25.54";
+const uint16_t MQTT_PORT = 1883;
+const char* MQTT_CLIENT_NAME = "ESPClient_1";
+
+// Topics
+const char* DEVICE_TYPE = "Light";
+const char* DEVICE_ID = "0";
+#define RELAY 14
+
+extern bool read_fp;
+
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
+
+void SuscribeMqtt()
+{
+  Serial.print("subscribed");
+  mqttClient.subscribe("voting/enableVoting");
+  mqttClient.subscribe("voting/alert");
+}
+
+String payload;
+void PublisMqtt(unsigned int data)
+{
+   payload = "";
+   payload = String(data);
+   mqttClient.publish("voting/identify/fingerprint", (char*)payload.c_str());
+}
+
+void PublisMqttString(char* topic, char* msg)
+{
+   mqttClient.publish(topic, msg);
+}
+
+String content = "";
+void OnMqttReceived(char* topic, byte* payload, unsigned int length) 
+{
+   Serial.print("Received on ");
+   Serial.print(topic);
+   Serial.print(": ");
+
+   content = "";   
+   for (size_t i = 0; i < length; i++) {
+      content.concat((char)payload[i]);
+   }
+   Serial.print(content);
+   Serial.println();
+  if(strcmp(topic, "voting/enableVoting")==0){
+    if((char)payload[0] == '1') {
+      read_fp=true;
+   }
+   else if ((char)payload[0] == '0') {
+      read_fp=false;
+   }  
+  }
+
+}
